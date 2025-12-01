e6e6996242601d4bb3c782d61a7a52295ad01026
MaciejSocha <maciek.socha1@gmail.com>
2025-12-01 15:12:38 +0100

    Part 1 of Day 1 - Done

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# AdventOfCode2025
-https://adventofcode.com/2025 solutions for 2025
+# AdventOfCode2024
+My adventofcode.com solutions for 2024 event!
+
+![Day01](https://img.shields.io/badge/Day1-Done-brightgreen.svg)
+
+![Day02](https://img.shields.io/badge/Day2-Waiting-lightgrey.svg)
+
+![Day03](https://img.shields.io/badge/Day3-Waiting-lightgrey.svg)
+
+![Day04](https://img.shields.io/badge/Day4-Waiting-lightgrey.svg)
+
+![Day05](https://img.shields.io/badge/Day5-Waiting-lightgrey.svg)
+
+![Day06](https://img.shields.io/badge/Day6-Waiting-lightgrey.svg)
+
+![Day07](https://img.shields.io/badge/Day7-Waiting-lightgrey.svg)
+
+![Day08](https://img.shields.io/badge/Day8-Waiting-lightgrey.svg)
+
+![Day09](https://img.shields.io/badge/Day9-Waiting-lightgrey.svg)
+
+![Day10](https://img.shields.io/badge/Day10-Waiting-lightgrey.svg)
+
+![Day11](https://img.shields.io/badge/Day11-Waiting-lightgrey.svg)
+
+![Day12](https://img.shields.io/badge/Day12-Waiting-lightgrey.svg)
---
+++ src/main/java/aoc/day01/Day1.java
@@ -0,0 +1,46 @@
+package aoc.day01;
+
+import utils.FileReader;
+
+public class Day1 {
+    public static void main(String[] args) {
+        String input = FileReader.read("src/main/resources/01data.txt");
+        int position = 50;
+        int zeroCount = 0;
+
+        for (String line : input.split("\\R")) {
+            char direction = line.charAt(0);
+            int distance = Integer.parseInt(line.split(String.valueOf(direction))[1]);
+
+            //full circles
+            int fullCirclesCount = distance / 100;
+            //zeroCount += fullCirclesCount;
+            distance -= fullCirclesCount * 100;
+
+            if (direction == 'L') {
+                if (position - distance < 0) {
+                    distance -= position;
+                    //zeroCount++;
+                    position = 100 - distance;
+                } else if (position - distance == 0) {
+                    position -= distance;
+                    zeroCount++;
+                }
+                else position -= distance;
+            }
+
+            if (direction == 'R') {
+                if (position + distance > 100) {
+                    distance -= 100 - position;
+                    //zeroCount++;
+                    position = distance;
+                } else if (position + distance == 100) {
+                    zeroCount++;
+                    position = 0;
+                }
+                else position += distance;
+            }
+        }
+        System.out.println(zeroCount);
+    }
+}
